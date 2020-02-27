e68a2b8bde2dfbf54cd4b8e99f882bec74247e94
beizhu <beizhu.wc@alibaba-inc.com>
2020-02-27 17:01:09 +0800

    feat: compute code directory by rax project type

---
+++ src/index.js
@@ -0,0 +1,187 @@
+/**
+ * @name plugin directory
+ * @param option: { data, filePath, config }
+ * - data: module and generate code Data
+ * - filePath: Pull file storage directory
+ * - config: cli config
+ */
+
+const fs = require('fs-extra');
+const path = require('path');
+const util = require('./util');
+
+function replaceLocalImports(panelValue, imports, fileName) {
+  let replacement = '../';
+  if (fileName === 'index') {
+    replacement = './components/';
+  }
+  imports.forEach(item => {
+    const newItem = item.replace('./', replacement);
+    panelValue = panelValue.replace(item, newItem);
+  });
+  return panelValue;
+}
+
+function replaceCssImport(panelValue, fileName) {
+  panelValue = panelValue.replace(
+    `import styles from './${fileName}.css';`,
+    `import styles from './index.css';`
+  );
+  return panelValue;
+}
+
+function collectImports(imports, panelImports) {
+  const realImports = panelImports
+    .filter(item => {
+      return item.indexOf('./') === -1;
+    })
+    .concat(imports);
+  return Array.from(new Set(realImports));
+}
+
+function getPageName(data) {
+  if (data && data.moduleData) {
+    if (data.moduleData.name) {
+      return data.moduleData.name;
+    } else if (data.moduleData.id) {
+      return 'page' + data.moduleData.id;
+    }
+  }
+  return 'page';
+}
+
+/**
+ * 源码生成后，将依赖的 npm 包写入 package.json
+ * @param {*} projectPath
+ * @param {*} imports
+ */
+function getPackageJSONPanel(projectPath, imports) {
+  const packages = imports.map(item => {
+    return item.match(/\'(.*)?\'/g)[0].slice(1, -1);
+  });
+  const packageJSONPath = util.findClosestFilePath(projectPath, 'package.json');
+  if (!fs.pathExistsSync(packageJSONPath)) {
+    return false;
+  }
+  let flag = false;
+  try {
+    const json = JSON.parse(fs.readFileSync(packageJSONPath).toString());
+    if (!json.dependencies) {
+      json.dependencies = {};
+    }
+    if (!json.devDependencies) {
+      json.devDependencies = {};
+    }
+    packages.forEach(name => {
+      if (!json.dependencies[name] && !json.devDependencies[name]) {
+        flag = true;
+        json.dependencies[name] = '*';
+      }
+    });
+    if (flag) {
+      return {
+        panelPath: path.resolve(packageJSONPath),
+        panelName: 'package.json',
+        panelValue: `${JSON.stringify(json, null, 2)}\n`,
+        panelType: 'json'
+      };
+    } else {
+      return null;
+    }
+  } catch (e) {
+    return null;
+  }
+}
+
+/**
+ * Rax 1.0 多页应用 && 单页应用源码生成更新路由信息 app.json
+ * @param {*} projectPath
+ * @param {*} pageName
+ */
+function getAppJSONPanel(projectPath, pageName, projectType) {
+  if (
+    projectType.type === util.PROJECT_TYPE.Rax1MultiApp.type ||
+    projectType.type === util.PROJECT_TYPE.Rax1SPAApp.type
+  ) {
+    const appJSONPath = path.join(projectPath, 'src/app.json');
+    if (!fs.pathExistsSync(appJSONPath)) {
+      return false;
+    }
+    try {
+      const json = JSON.parse(fs.readFileSync(appJSONPath).toString());
+      if (!json.routes) {
+        json.routes = [];
+      }
+      json.routes.push({
+        path: `/${pageName}`,
+        source: `pages/${pageName}/index`
+      });
+      return {
+        panelPath: appJSONPath,
+        panelName: 'app.json',
+        panelValue: `${JSON.stringify(json, null, 2)}\n`,
+        panelType: 'json'
+      };
+    } catch (e) {
+      return null;
+    }
+  }
+}
+
+const pluginHandler = async options => {
+  let { filePath: projectPath, data } = options;
+  let imports = [];
+  const isTSProject = fs.pathExistsSync(path.join(projectPath, 'tsconfig.json'));
+  const projectType = util.getProjectType(projectPath);
+  let codeDirectory = '';
+  let pageName = getPageName(data);
+  try {
+    codeDirectory = util.getCodeDirectory(projectType, projectPath, pageName);
+    fs.ensureDirSync(codeDirectory);
+  } catch (error) {
+    codeDirectory = projectPath;
+  }
+
+  const panelDisplay = data.code.panelDisplay;
+  data.code.panelDisplay = panelDisplay.map(item => {
+    try {
+      let { panelName, panelValue, panelImports = [] } = item;
+      let filePath = '';
+      const fileName = panelName.split('.')[0];
+      const fileType = util.optiFileType(panelName.split('.')[1], isTSProject, projectType);
+      if (fileName !== 'index' && fileName !== 'context') {
+        filePath = path.resolve(codeDirectory, 'components', fileName, `index.${fileType}`);
+      } else {
+        filePath = path.resolve(codeDirectory, `${fileName}.${fileType}`);
+      }
+      panelValue = replaceCssImport(panelValue, fileName);
+      panelValue = replaceLocalImports(panelValue, panelImports, fileName);
+      imports = collectImports(imports, panelImports);
+      return {
+        ...item,
+        panelValue,
+        panelPath: filePath
+      };
+    } catch (error) {}
+  });
+
+  // 解析 package.json
+  const pkgPanel = getPackageJSONPanel(projectPath, imports);
+  if (pkgPanel) {
+    data.code.panelDisplay.push(pkgPanel);
+  }
+  // 解析 app.json
+  const appPanel = getAppJSONPanel(projectPath, pageName, projectType);
+  if (appPanel) {
+    data.code.panelDisplay.push(appPanel);
+  }
+  // codeDiff 标识插件做代码 diff
+  data.code.codeDiff = true;
+  return options;
+};
+
+module.exports = (...args) => {
+  return pluginHandler(...args).catch(err => {
+    console.log(err);
+  });
+};
---
+++ src/util.js
@@ -0,0 +1,114 @@
+const fs = require('fs-extra');
+const parentDirs = require('parent-dirs');
+const path = require('path');
+const { PROJECT_TYPE } = require('./constant');
+
+function getCodeDirectory(projectType, projectPath, pageName) {
+  let codeDirectory = '';
+  switch (projectType.type) {
+    case PROJECT_TYPE.Rax1MultiApp.type:
+    case PROJECT_TYPE.Rax1SPAApp.type: {
+      codeDirectory = path.resolve(projectPath, `src/pages/${pageName}`);
+      break;
+    }
+    case PROJECT_TYPE.Rax1Comp.type:
+    case PROJECT_TYPE.Rax1CompUI.type: {
+      codeDirectory = path.resolve(projectPath, 'src');
+      break;
+    }
+    case PROJECT_TYPE.Rax1TBEMod.type: {
+      // 天马模块目录
+      codeDirectory = path.resolve(projectPath, 'src/mobile');
+      if (!fs.pathExistsSync(codeDirectory)) {
+        // 天马组件目录
+        codeDirectory = path.resolve(projectPath, 'src');
+      }
+      break;
+    }
+    default: {
+      // 默认当成模块导出
+      codeDirectory = path.resolve(projectPath, pageName);
+      break;
+    }
+  }
+  return codeDirectory;
+}
+
+function getProjectType(projectPath) {
+  const projectBuildPath = path.join(projectPath, 'build.json');
+  const projectABCPath = path.join(projectPath, 'abc.json');
+  if (fs.existsSync(projectBuildPath)) {
+    try {
+      const jsonString = fs.readFileSync(projectBuildPath).toString();
+      if (jsonString.indexOf('build-plugin-rax-app') > -1) {
+        if (jsonString.indexOf('build-plugin-rax-multi-pages') > -1) {
+          return PROJECT_TYPE.Rax1MultiApp;
+        } else {
+          return PROJECT_TYPE.Rax1SPAApp;
+        }
+      } else if (jsonString.indexOf('build-plugin-rax-component') > -1) {
+        if (jsonString.indexOf('build-plugin-multi-demo-portal') > -1) {
+          return PROJECT_TYPE.Rax1CompUI;
+        } else {
+          return PROJECT_TYPE.Rax1Comp;
+        }
+      }
+    } catch (e) {
+      console.error('解析 build.json 出错');
+    }
+  } else if (fs.existsSync(projectABCPath)) {
+    try {
+      const json = JSON.parse(fs.readFileSync(projectABCPath).toString());
+      if (json.builder && typeof json.builder === 'string') {
+        if (json.type === 'tbe-mod') {
+          return PROJECT_TYPE.Rax1TBEMod;
+        }
+      }
+    } catch (e) {
+      console.error('解析 abc.json 出错');
+    }
+  }
+  return PROJECT_TYPE.Other;
+}
+
+function optiFileType(fileType, isTS, projectType) {
+  if (isTS) {
+    if (fileType === 'js') {
+      return 'ts';
+    }
+    if (fileType === 'jsx') {
+      return 'tsx';
+    }
+  } else if (['js', 'jsx'].indexOf(fileType) > -1) {
+    return projectType.jsType;
+  }
+  return fileType;
+}
+
+function findClosestFilePath(baseDir, fileName, maxLayer = 5) {
+  try {
+    let filePath = '';
+    let dirs = parentDirs(baseDir) || [];
+    let length = Math.min(dirs.length, maxLayer);
+    let index = 0;
+    while (index < length) {
+      let curPath = path.join(dirs[index], fileName);
+      if (fs.pathExistsSync(curPath)) {
+        filePath = curPath;
+        break;
+      }
+      index++;
+    }
+    return filePath;
+  } catch (e) {
+    return '';
+  }
+}
+
+module.exports = {
+  PROJECT_TYPE,
+  getProjectType,
+  getCodeDirectory,
+  findClosestFilePath,
+  optiFileType
+};
